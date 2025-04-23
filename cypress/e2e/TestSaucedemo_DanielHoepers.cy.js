9204f6dda2b5352605500197ebae0454eaf56aac
Daniel Hoepers <danielrch.hoepers@gmail.com>
2025-04-22 21:35:07 -0300

    Update TestSaucedemo_DanielHoepers.cy.js

--- cypress/e2e/TestSaucedemo_DanielHoepers.cy.js
+++ cypress/e2e/TestSaucedemo_DanielHoepers.cy.js
@@ -3,25 +3,7 @@ describe('Autenticação', () => {
         cy.visit('https://www.saucedemo.com/')
     })
 
-    it('Deve realizar o login com sucesso', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-
-        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
-        cy.contains('Products').should('be.visible')
-    })
-
-    it('Deve realizar o logout', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('#react-burger-menu-btn').click()
-        cy.get('[data-test="logout-sidebar-link"]').click()
-
-        cy.url().should('eq', 'https://www.saucedemo.com/')
-        cy.contains('Login').should('be.visible')
-    })
+    
 
     it('Deve exibir mensagem de erro ao tentar logar com usuário bloqueado', () => {
         cy.get('[data-test="username"]').type('locked_out_user')
