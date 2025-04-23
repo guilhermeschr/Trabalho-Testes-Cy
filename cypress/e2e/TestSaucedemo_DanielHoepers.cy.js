d38e7e9af58ec4053004825c928b577245daae0e
Daniel Hoepers <danielrch.hoepers@gmail.com>
2025-04-22 21:38:50 -0300

    Update TestSaucedemo_DanielHoepers.cy.js

--- cypress/e2e/TestSaucedemo_DanielHoepers.cy.js
+++ cypress/e2e/TestSaucedemo_DanielHoepers.cy.js
@@ -3,7 +3,6 @@ describe('Autenticação', () => {
         cy.visit('https://www.saucedemo.com/')
     })
 
-    
 
     it('Deve exibir mensagem de erro ao tentar logar com usuário bloqueado', () => {
         cy.get('[data-test="username"]').type('locked_out_user')
@@ -12,5 +11,29 @@ describe('Autenticação', () => {
 
         cy.get('[data-test="error"]').should('be.visible')
             .and('contain', 'Sorry, this user has been locked out.')
+    })
+
+    
+
+
+    
+})
+
+describe('Página de Produtos', () => {
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+    })
+
+    it('Deve exibir produtos na página', () => {
+
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+        cy.contains('Products').should('be.visible')
+        cy.get('.inventory_item').should('have.length.greaterThan', 0)
+        cy.get('.inventory_item').each(($el) => {
+        cy.wrap($el).find('[data-test="add-to-cart-sauce-labs-backpack"]').should('be.visible')
+        })
     })
 })
