2c16d3d72169fe0e42cd87f7d9c5693c2d90603e
Daniel Hoepers <danielrch.hoepers@gmail.com>
2025-04-22 22:07:00 -0300

    Update TestSaucedemo_DanielHoepers.cy.js

--- cypress/e2e/TestSaucedemo_DanielHoepers.cy.js
+++ cypress/e2e/TestSaucedemo_DanielHoepers.cy.js
@@ -14,7 +14,7 @@ describe('Autenticação', () => {
     });
   });
   
-  describe('Inventário – Validando Lista e Ordenação por Nome', () => {
+  describe('Inventário – Validando Lista', () => {
     beforeEach(() => {
       cy.visit('https://www.saucedemo.com/');
       cy.get('[data-test="username"]').type('standard_user');
